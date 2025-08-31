9a15dd1246d4af4cd93bafb6d1e87312f9d54de0
veetaragpatil <165647926+veetaragpatil@users.noreply.github.com>
2025-08-31 21:57:58 +0530

    Create Prime_Factorization_of_a_Number.py

---
+++ day17/Prime_Factorization_of_a_Number.py
@@ -0,0 +1,15 @@
+def prime_factorization(n):
+    factors = []
+    while n % 2 == 0:
+        factors.append(2)
+        n //= 2
+    i = 3
+    while i * i <= n:
+        while n % i == 0:
+            factors.append(i)
+            n //= i
+        i += 2
+    if n > 2:
+        factors.append(n)
+    return factors
+print(prime_factorization(18)) 
